766b4aaed9048b8ded97b0b318d85d9ed33cea1b
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-02-20 23:11:32 +0530

    resolved conflicts

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 //use express.json() to get data into json format
 app.use(express.json());
-//Port 
+//Port
 const PORT = process.env.PORT || 5500;
 
 //use cors
 app.use(cors());
 
 //import routes
-const TodoItemRoute = require('./routes/todoItems');
-
+const TodoItemRoute = require("./routes/todoItems");
 
 //connect to mongodb ..
-mongoose.connect(process.env.DB_CONNECT)
-.then(()=> console.log("Database connected"))
-.catch(err => console.log(err))
+mongoose
+  .connect(
+    `mongodb+srv://lovishbansal330:${process.env.password}@todo-cluster.jk9a1bg.mongodb.net/todolistDB`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
 
-
-app.use('/', TodoItemRoute);
-
-
+app.use("/", TodoItemRoute);
 
 //connect to server
-app.listen(PORT, ()=> console.log("Server connected") );
+app.listen(PORT, () => console.log("Server connected"));
